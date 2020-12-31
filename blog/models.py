1e4d2d9274dfc51dd2f3402de976307797fa4433
masoud jahani <jahani.m180@gmail.com>
2020-12-31 13:26:31 +0330

    make models Expense and some beatification

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=20)
     body = models.CharField(max_length=200)
-    text = models.CharField(max_length=300)
+    text = models.TextField()
+    def __str__(self):
+    	return self.title
     
-    
+class Expense(models.Model):
+    text = models.CharField(max_length=255)    
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+    	return "{}-{}".format(self.date,self.text)   
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.models import Post
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hi masoud")
+    posts = Post.objects.all()
+    return render(request,'index.html',{'posts':posts})
+    #return HttpResponse("hi masoud")
     #all_post = Post.object.all()
 
 def test(request):
